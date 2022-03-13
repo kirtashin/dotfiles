49c8bc502a3a1fa1c1108f59a1cd7e6d613e74bc
Sam <smartinez@bioksan.com>
2022-03-14 00:12:29 +0100

    Modificada estructura de dotfiles. Añadidos symlinks y alias

---
+++ shell/aliases.sh
@@ -0,0 +1,13 @@
+alias ll = "exa -l"
+alias la = "exa -la"
+
+#Jumps
+alias ~="cd ~"
+alias dotfiles="cd ~/.dotfiles"
+
+#Util
+alias copy="pbcopy"
+alias i="(idea $PWD &>/dev/null &)"
+alias o.="open ."
+
+
